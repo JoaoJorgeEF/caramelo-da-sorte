40f3f9019b05b0a5487e129a5ad43de0ccbfeb7d
João Jorge <52219312+JoaoJorgeEF@users.noreply.github.com>
2022-06-07 14:10:29 -0300

    Commit inicial

---
+++ src/main/java/br/edu/ifpb/pweb2/caramelodasorte/CarameloDaSorteApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.caramelodasorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarameloDaSorteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarameloDaSorteApplication.class, args);
+    }
+
+}
